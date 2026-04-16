785398231b039813c704c2341bc5e8fc820c8440
Dev23504 <devrajsinghsisodiya722gmail.com>
2026-04-16 17:25:22 +0530

    2

--- index.js
+++ index.js
@@ -25,11 +25,46 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-  })
-  .catch((err) => console.log("Mongo Error:", err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log("Server running on", PORT);
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket connected:", socket.id);
+
+  socket.on("join", (userId) => {
+    socket.join(userId);
+    onlineUsers[userId] = socket.id;
+    io.emit("onlineUsers", Object.keys(onlineUsers));
+  });
+
+  socket.on("sendMessage", async (data) => {
+    try {
+      const msg = await Message.create({
+        sender: data.sender,
+        receiver: data.receiver,
+        text: data.text || "",
+        file: data.file || "",
+        type: data.type || "text",
+      });
+
+      io.to(data.receiver).emit("receiveMessage", msg);
+      io.to(data.sender).emit("receiveMessage", msg);
+    } catch (err) {
+      console.log("Message Error:", err);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    for (let user in onlineUsers) {
+      if (onlineUsers[user] === socket.id) {
+        delete onlineUsers[user];
+        break;
+      }
+    }
+    io.emit("onlineUsers", Object.keys(onlineUsers));
+  });
 });
